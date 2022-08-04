0e3402d28dfbbdcc7ee55e332d2c93cd374904e7
Adrian Arnett <adrianarnett0@gmail.com>
2022-08-03 22:28:09 -0500

    Loop now properly counts new emails

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 
 import requests
 import sched
+import datetime
 import time
 import configparser
+import urllib.parse
 
 # libs
 import msal
@@ -17,9 +19,9 @@ class AuthenticationError(Exception):
     pass
 
 
+APPLICATION_SEPARATOR = "----------------------------------------------------------------"
 APPLICATION_REAUTH_TIME = 0.8  # the factor to multiply expires_in to renew the authentication token - provides margin for error
 APPLICATION_SLEEP_TIME = 15  # check for new emails every 20 seconds
-
 # read oauth2 token from config file
 config = configparser.ConfigParser()
 config.read("./settings.cfg")
@@ -32,6 +34,7 @@ MS_IDENTITY_SCOPE = config["microsoft_identity"]["scope"]
 APPLICATION_MAILBOX = f'{config["application"]["mailbox"]}@{AZURE_DOMAIN}'
 
 scheduler = sched.scheduler()  # init scheduler
+app_delta_link = "https://graph.microsoft.com/v1.0/users/83eed5d0-22aa-4ee3-8d9a-0f5408f9d365/mailFolders/Inbox/messages/delta"
 
 
 def authenticate():
@@ -51,26 +54,37 @@ def authenticate():
     return response
 
 
+def request_get_authenticated(url):
+    response = requests.get(url, headers={"Authorization": f"Bearer {AUTH_KEY}"})
+    response.raise_for_status()
+    return response.json()
+
+
 def obtain_all_emails():  # TODO: Fix this to use a mail enabled security group
-    response = requests.get("https://graph.microsoft.com/v1.0/users/83eed5d0-22aa-4ee3-8d9a-0f5408f9d365/messages",
-                            headers={"Authorization": f"Bearer {AUTH_KEY}"})
-    return response.json()
+    return request_get_authenticated(
+        "https://graph.microsoft.com/v1.0/users/83eed5d0-22aa-4ee3-8d9a-0f5408f9d365/messages")
 
 
-def obtain_email_delta():
-    response = requests.get(
-        "https://graph.microsoft.com/v1.0/users/83eed5d0-22aa-4ee3-8d9a-0f5408f9d365/mailFolders/Inbox/messages/delta",
-        headers={"Authorization": f"Bearer {AUTH_KEY}"})
-    return response.json()
+def check_new_emails():
+    global app_delta_link
+    response = request_get_authenticated(app_delta_link)  # TODO: handle @odata.nextLink (exists if pages exceed limit)
+    email_count = len(response['value'])
+    app_delta_link = response['@odata.deltaLink']
+    return email_count
 
 
+print(APPLICATION_SEPARATOR)
+print(str(datetime.datetime.now()))
+print("starting application...")
 AUTH_KEY = authenticate()["access_token"]
+check_new_emails()  # ignore pre-existing emails
 
 while True:
+    print(APPLICATION_SEPARATOR)
+    print(str(datetime.datetime.now()))
     print("checking authentication token...")
     scheduler.run(False)  # check for scheduled events - non-blocking
     print("checking for new emails...")
-    print(obtain_email_delta())
+    print(f"{check_new_emails()} new emails")  # TODO: Seems like an email can persist in some conditions?
     print("end check")
     time.sleep(APPLICATION_SLEEP_TIME)
-
